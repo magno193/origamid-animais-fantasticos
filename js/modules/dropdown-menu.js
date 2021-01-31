7d44aa1d8c730d19550730763c9ed03f4beaf5b4
Alexandre Ferreira <alexmlf93@gmail.com>
2021-01-31 14:44:08 -0300

    Lógica de outside click em dropdown

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,3 +1,5 @@
+import outsideClick from './outsideclick.js';
+
 export default function initDropdownMenu() {
   const dropdownMenus = document.querySelectorAll('[data-dropdown]');
 
@@ -10,5 +12,8 @@ export default function initDropdownMenu() {
   function handleClick(e) {
     e.preventDefault();
     this.classList.toggle('active');
+    outsideClick(this, ['touchstart', 'click'], () => {
+      this.classList.remove('active');
+    });
   }
 };
