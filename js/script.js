edb2dc434d0376867938a22770b26a3a411a8308
Alexandre Ferreira <alexmlf93@gmail.com>
2021-01-31 16:37:22 -0300

    Efeito de menu em mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import initAccordion from './modules/accordion.js';
 import initAnimationScroll from './modules/animation-scroll.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
 import initModal from './modules/modal.js';
 import initSmoothScroll from './modules/smooth-scroll.js';
 import initTabNav from './modules/tabnav.js';
@@ -13,3 +14,4 @@ initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
+initMenuMobile();
