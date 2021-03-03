ec8bd5ff56f584ccbf99c8f15be2fbbf597662ed
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-03 13:49:17 -0300

    refactoring of dropdown-menu.js using class

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,17 +1,38 @@
 import outsideClick from './outsideclick.js';
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+    // define touchstart and click as default paramater for events
+    // if undefined
+    this.events = !events ? ['touchstart', 'click'] : events;
+    this.activeClass = 'active';
+    // binding
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
 
-  function handleClick(e) {
-    e.preventDefault();
-    this.classList.toggle('active');
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove('active');
+  // activate the dropdownmenu and add the function that
+  // watches the click outside of it
+  activeDropdownMenu(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    element.classList.toggle(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((item) => menu.addEventListener(item, handleClick));
-  });
+  // add the events to the dropdownmenu
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      this.events.forEach((item) => menu.addEventListener(item, this.activeDropdownMenu));
+    });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownMenusEvent();
+    }
+    return this;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 import Accordion from './modules/accordion.js';
 import TabNavigation from './modules/tabnav.js';
 import ScrollAnimation from './modules/scroll-animation.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
 import fetchBitCoin from './modules/fetch-bitcoin.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import Modal from './modules/modal.js';
@@ -9,6 +8,7 @@ import initOperation from './modules/operation.js';
 import SmoothScroll from './modules/smooth-scroll.js';
 import ToolTip from './modules/tooltip.js';
 import fetchAnimals from './modules/fetch-animals.js';
+import DropdownMenu from './modules/dropdown-menu.js';
 
 const smoothScroll = new SmoothScroll('[data-menu="suave"] a[href^="#"]');
 smoothScroll.init();
@@ -32,7 +32,8 @@ toolTip.init();
 const scrollAnimation = new ScrollAnimation('[data-animation="scroll"]');
 scrollAnimation.init();
 
-initDropdownMenu();
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
 initMenuMobile();
 initOperation();
 fetchAnimals('../mock/animalsapi.json', '.numbers-grid');
