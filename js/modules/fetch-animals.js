870eda6cc5bded7381af5393ee2ef15b4c1e9e51
Alexandre Ferreira <alexmlf93@gmail.com>
2021-03-02 22:42:34 -0300

    refactoring of fetch-animals.js

--- js/modules/fetch-animals.js
+++ js/modules/fetch-animals.js
@@ -1,30 +1,34 @@
 import AnimateNumbers from './animate-numbers.js';
 
-export default function initFetchAnimals() {
+export default function fetchAnimals(url, target) {
   function createAnimalHtml(animal) {
     const div = document.createElement('div');
     div.classList.add('number-animal');
     div.innerHTML = `<h3>${animal.specie}</h3><span data-number>${animal.total}</span>`;
-
     return div;
   }
 
-  async function fetchAnimals(url) {
+  function animateAnimalsNumbers() {
+    const animateNumbers = new AnimateNumbers('[data-number]', '.numbers', 'ativo');
+    animateNumbers.init();
+  }
+
+  const numberGrid = document.querySelector(target);
+  function appendAnimal(animal) {
+    const divAnimal = createAnimalHtml(animal);
+    numberGrid.appendChild(divAnimal);
+  }
+
+  // fetch the animals and create the elements
+  async function createAnimals() {
     try {
       const animalsResponse = await fetch(url);
       const animalsJSON = await animalsResponse.json();
-      const numberGrid = document.querySelector('.numbers-grid');
-
-      animalsJSON.forEach((animal) => {
-        const divAnimal = createAnimalHtml(animal);
-        numberGrid.appendChild(divAnimal);
-      });
-      const animateNumbers = new AnimateNumbers('[data-number]', '.numbers', 'ativo');
-      animateNumbers.init();
+      animalsJSON.forEach((animal) => appendAnimal(animal));
+      animateAnimalsNumbers();
     } catch (error) {
       console.log(error);
     }
   }
-
-  fetchAnimals('./mock/animalsapi.json');
+  return createAnimals();
 }
